f76b47c4113c91d2517bed994b8cf0492d874fbc
Luckaa <lucasilva137@gmail.com>
2018-10-22 01:31:21 -0200

    deletar finalizado

--- src/components/Carro.vue
+++ src/components/Carro.vue
@@ -3,17 +3,8 @@
     <div style="padding:10px" >
       <v-btn color="info" v-if="!mostrarForm" @click="exibirForm">Adicionar carros</v-btn>
     </div>
-      <v-dialog v-model="excluirMesmo" width="500"  > 
-              <v-card>
-                  <v-card-title class="headline">Deseja mesmo excluir esse carro?</v-card-title>
-                  <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" flat @click.native="dialog = false">Disagree</v-btn>
-                  <v-btn color="green darken-1" flat @click.native="dialog = false">Agree</v-btn>
-                  </v-card-actions>
-              </v-card>
-      </v-dialog>
-    <v-dialog v-model="mostrarForm" width="500"  > 
+     
+    <v-dialog v-model="mostrarForm" width="400"  > 
 
       <v-card>
         <v-card-title class="headline grey lighten-2" primary-title>
@@ -24,14 +15,13 @@
         
 
         <v-form ref="form" v-if="mostrarForm" v-model="valid" lazy-validation style="padding:10px">
-          <v-text-field v-model="carro.foto" :rules="nomeRules" :counter="30" label="Foto" required></v-text-field>
+          <v-text-field v-model="carro.foto" label="Foto" required></v-text-field>
           <v-text-field v-model="carro.modelo" label="Modelo" required></v-text-field>
           <v-text-field v-model="carro.ano" label="Ano" required></v-text-field>
           <v-text-field v-model="carro.kilometragem" label="Quilometragem" required></v-text-field>
           <v-text-field v-model="carro.combustivel" label="Combustivel" required></v-text-field>
           <v-text-field v-model="carro.cor" label="Cor" required></v-text-field>
           <v-text-field v-model="carro.conservacao" label="Estado de Conservação" required></v-text-field>
-          <v-text-field v-model="carro.obs" label="Observações" required></v-text-field>
 
           <v-btn :disabled="!valid" @click="submit">
             salvar
@@ -43,38 +33,50 @@
     </v-dialog>
 
 
-    <v-data-table :headers="headers" :items="carros" hide-actions class="elevation-1">
+    <v-data-table :headers="headers" :items="carros" hide-actions  :loading="true" light class="elevation-1">
       <template slot="items" slot-scope="props">
          <!-- {{items}} -->
          <!-- {{props}} -->
       
+        <td class="text-xs-left"> {{ (props.item.modelo)}} </td>
         <td class="text-xs-left">{{ props.item.ano }}</td>
         <td class="text-xs-left"> {{ props.item.combustivel }}</td>
-        <td class="text-xs-left"> {{ (props.item.conservacao)}}</td>
-        <td class="text-xs-left"> {{ (props.item.cor)}}</td>
-        <td class="text-xs-left"><img style="width:80px;height:80px;overflow:hidden" :src="props.item.foto"></td>
         <td class="text-xs-left"> {{ (props.item.kilometragem) }}</td>
-        <td class="text-xs-left"> {{ (props.item.modelo)}}</td>
-        <td class="text-xs-left"> {{ (props.item.obs)}}</td>
-    
+        <td class="text-xs-left"> {{ (props.item.cor)}}</td>
+        <td class="text-xs-left"> {{ (props.item.conservacao)}}</td>
+        <td class="text-xs-left"><img style="width:80px;height:80px;overflow:hidden" :src="props.item.foto"></td>
+
         <td>
           <v-flex>
             <v-btn flat icon color="black" v-if="!excluirMesmo" @click="exibirExcluir">
               <v-icon v-model="excluirMesmo">delete</v-icon>
             </v-btn>
 
+
             <v-btn flat icon color="blue">
               <v-icon @click="editarCarro(props.items)">edit</v-icon>
             </v-btn>
           </v-flex>
         </td>
+
+          <v-dialog v-model="excluirMesmo" width="300"  > 
+              <v-card>
+                  <v-card-title class="headline">Deseja mesmo excluir esse carro?</v-card-title>
+                  <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="red darken-1" flat @click="exibirExcluir()">Não</v-btn>
+                  <v-btn color="green darken-1" flat @click="deletarCarro(props.item)">Sim</v-btn>
+                  </v-card-actions>
+              </v-card>
+      </v-dialog>
+      
+
       </template>
     </v-data-table>
 
 
   </div>
 </template>
-
 <script>
   import API from '../lib/API';
   export default {
@@ -112,9 +114,9 @@
             API.editarCarros(this.carro)
               .then(response => {
                 if (response) {
-                  this.alerta("carro editado com Sucesso!", "success");
+                  console.log("carro editado com Sucesso!", "success");
                 } else {
-                  this.alerta("Erro ao editar carro!", "error");
+                  console.log("Erro ao editar carro!", "error");
                 }
                 this.clear()
                 this.load()
@@ -137,14 +139,15 @@
       },
 
       deletarCarro(carro) {
-        console.log("teste");
+        console.log(carro)
+        this.exibirExcluir()
         API.deletarCarros(carro)
-          .then(response => {
+        .then(response => {
             if (response) {
-              this.alerta("carros removido com Sucesso!", "success");
+              console.log("Carro removido com Sucesso!", "success");
               this.load()
             } else {
-              this.alerta("Erro ao remover carros!", "error");
+              console.log("Erro ao remover carro!", "error");
             }
           });
       },
@@ -153,35 +156,21 @@
         this.carros = carros; 
         this.mostrarForm = true;
       },
-
-      confirmarExclusao(flag, carros) {
-        console.log(flag);
-        console.log(carros);
-
-        if (flag) {
-          this.deletarCarro(carros);
-        }
-        this.dialog = false;
-      },
-
-      alerta(msg, type) {
-        let msgObj = {
-          msg: msg,
-          type: type,
-          value: true
-        };
-
-        this.$store.commit('CHANGE_MSG', msgObj);
-      }
     },
 
     data() {
       return {
         carro: {},
+        carros: [],
+        valid: true,
         dialog: false,
         mostrarForm: false,
         excluirMesmo: false,
         headers: [{
+            text: 'Modelo',
+            value: 'modelo'
+          },
+          {
             text: 'Ano',
             value: 'ano'
           },
@@ -190,36 +179,26 @@
             value: 'combustivel'
           },
           {
-            text: 'conservacao',
-            value: 'conservacao'
-          },
-          {
-            text: 'cor',
-            value: 'cor'
-          },
-          {
-            text: 'foto',
-            value: 'foto'
-          },
-            {
-            text: 'kilometragem',
+            text: 'Quilometragem',
             value: 'kilometragem'
           },
-            {
-            text: 'modelo',
-            value: 'modelo'
-          },
           {
-            text: 'obs',
-            value: 'obs'
+            text: 'Cor',
+            value: 'cor'
+          },
+            {
+            text: 'Conservação',
+            value: 'conservacao'
+          },
+            {
+            text: 'Foto',
+            value: 'foto'
           },
           {
             text: 'Ações',
             value: ''
           }
         ],
-        carros: [],
-        valid: true,
         nomeRules: [
           v => !!v || 'Nome é Obrigatório!',
           v => (v && v.length <= 5000) || 'Nome deve ter no máximo 1000 characters!'
